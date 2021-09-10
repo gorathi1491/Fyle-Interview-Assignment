9d0564cb935df9aeb7d20e038361b5c2152335b8
Govind Rathi <79400921+gorathi1491@users.noreply.github.com>
2021-09-10 17:07:33 +0530

    Fyle-Interview-Assignment
    Fyle Data Extraction Challenge

---
+++ extract.py
@@ -0,0 +1,55 @@
+# Your imports go here
+import logging
+import json
+import os
+import re
+logger = logging.getLogger(__name__)
+
+'''
+    Given a directory with receipt file and OCR output, this function should extract the amount
+
+    Parameters:
+    dirpath (str): directory path containing receipt and ocr output
+
+    Returns:
+    float: returns the extracted amount
+
+'''
+def extract_amount(dirpath: str) -> float:
+    logger.info('extract_amount called for dir %s', dirpath)
+    # your logic goes here
+
+    # Extraction of the OCR file
+    ocr_file = os.path.join(dirpath,'ocr.json')
+    # Intialisation of the variable
+    text_data = []
+    amt = 0.0
+    act_amt = 0.0
+
+    # Reading of the json file in the particular OCR file
+    with open(ocr_file, mode='r', encoding="utf-8") as f:
+        data = (json.load(f)).get("Blocks")
+
+    # We have all our Required Data in the Text so we will take only that Part from the data
+    for i in data:
+        # Since it's possible that Text might not be present in everywhere use try and except to avoid errors
+        try:
+            text_data.append(i['Text'].upper()) # Conversion to upper because Python is case sensitive
+        except KeyError:
+            continue
+
+
+    # To extract the amount from the JSON File
+    for dt in text_data:
+        # In case amount consist of amount like 2,000,000 we are converting it to 2000000
+        if "," in dt:
+            dt = dt.replace(",",'')
+
+        # Value is the variable which take integer part from the data
+        value = re.findall(r"[+-]?\d+\.\d+", dt)
+
+        # We want the total price and in every bill the maximum float value will the amount
+        if len(value) > 0:
+            amt = float(value[0])
+            act_amt = max(act_amt,amt)
+    return act_amt
